7039f64472a72cbabdf261ccae7c2ec71152b2aa
Vlad <clay.zenx@gmail.com>
2022-04-28 11:32:03 +0700

    Complete

--- test.js
+++ test.js
@@ -13,57 +13,52 @@
  * @author		User Name 
  * @version		v.1.0 (dd/mm/yyyy)
  */
-class Api
-{
-	constructor() 
-	{
+class Api {
+  constructor() {
 
-	}
+  }
 
 
-	/**
-	 * Заполняет строковый шаблон template данными из объекта object
-	 *
-	 * @author		User Name 
-	 * @version		v.1.0 (dd/mm/yyyy)
-	 * @param		{object} object
-	 * @param		{string} template
-	 * @return		{string}
-	 */
-	get_api_path(object, template)
-	{
-		let result = '';
-
-		/* Здесь ваш код */
-
-		return result;
-	}
+  /**
+   * Заполняет строковый шаблон template данными из объекта object
+   *
+   * @author		User Name 
+   * @version		v.1.0 (dd/mm/yyyy)
+   * @param		{object} object
+   * @param		{string} template
+   * @return		{string}
+   */
+  get_api_path(object, template) {
+    return template.split('/')
+      .map(str => str.replace(/%\w+%/, object[str.replaceAll('%', '')]))
+      .map(str => str.replace(' ', '%20'))
+      .join('/');
+  }
 }
 
 
 let user =
 {
-	id		: 20,
-	name	: 'John Dow',
-	role	: 'QA',
-	salary	: 100
+  id: 20,
+  name: 'John Dow',
+  role: 'QA',
+  salary: 100
 };
 
 let api_path_templates =
-[
-	"/api/items/%id%/%name%",
-	"/api/items/%id%/%role%",
-	"/api/items/%id%/%salary%"
-];
+  [
+    "/api/items/%id%/%name%",
+    "/api/items/%id%/%role%",
+    "/api/items/%id%/%salary%"
+  ];
 
 let api = new Api();
 
-let api_paths = api_path_templates.map((api_path_template) =>
-{
-	return api.get_api_path(user, api_path_template);
+let api_paths = api_path_templates.map((api_path_template) => {
+  return api.get_api_path(user, api_path_template);
 });
 
 console.log(JSON.stringify(api_paths));
 
 // Ожидаемый результат
-let expected_result = ["/api/items/20/John%20Dow","/api/items/20/QA","/api/items/20/100"];
+let expected_result = ["/api/items/20/John%20Dow", "/api/items/20/QA", "/api/items/20/100"];
